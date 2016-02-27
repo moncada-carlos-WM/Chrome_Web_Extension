71b1f3f6356d18c8c2576f3b498d5c7ef6185f24
Carlos Moncada Garcia <carlos.garcia@west-mec.org>
2016-02-26 18:10:45 -0700

    My Extension

---
+++ Coal.js
@@ -0,0 +1,9 @@
+/**
+ * Created by session2 on 2/19/16.
+ */
+chrome.tabs.executeScript(null, {code: "alert('Deleting in 3,2,1...')"});
+
+console.log("Logged in" +
+    "");
+//chrome.tabs.executeScript(null, {code: "alert('test')"});
+alert("Deleting in 3,2,1...");
